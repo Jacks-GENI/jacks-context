b55291a3c28d3fea793a41c51a2d564bef727a45
Tom Mitchell <tmitchell@bbn.com>
2015-07-17 10:47:01 -0400

    Note that ExoSM is not included.
    Avoid a needless search to figure out which aggregate is missing from
    the list. It's ExoSM.

--- portal.sh
+++ portal.sh
@@ -5,6 +5,8 @@
                      --out portal-config.json \
                      --ir-since 2014-01-01
 
+# Note: ExoSM is excluded, it doesn't work properly with
+# jacks-context.
 python jacks-context.py --basic \
                         --config portal-config.json \
                         --output jacks-context.json \
